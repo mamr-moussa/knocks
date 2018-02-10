26b9231a9740772ba334e4ecb9f52a7f81ab9269
= <=>
2018-02-10 12:13:04 +0200

    46

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,49 @@
 <!DOCTYPE html>
 <html class = "knocks_color_kit">
-
-
   <head  prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-      <?php
-      if(auth()->check()){
-      $log = new App\User_log();
-      $log->addUserLog(auth()->user()->id , Request::url());
-      }
-      if(!auth()->check()){
-      $log = new App\User_log();
-      $log->addAnanymousLog(Request::url());
+    <?php
+    if(auth()->check()){
+    $log = new App\User_log();
+    $log->addUserLog(auth()->user()->id , Request::url());
     }
-  ?>
-  <meta property="fb:app_id" content="1796023703741381" /> 
-  <meta property="og:type"   content="website" /> 
-  <meta property="og:url"    content="https://knocksapp.com" /> 
-  <meta property="og:title"  content="Knocks" /> 
-  <meta property="og:image"  content="https://knocksapp.com/snaps/knocks_logo.png" /> 
-  
-  <meta name="google-site-verification" content="tsUIFUH4rGfxzV0M-SOdbUhgUTrwisWXmhNZpda_pVk" />
-
+    if(!auth()->check()){
+    $log = new App\User_log();
+    $log->addAnanymousLog(Request::url());
+    }
+    ?>
+    <!--OG TAGS-->
+    <meta property="fb:app_id" content="1796023703741381" />
+    <meta property="og:type"   content="website" />
+    <meta property="og:url"    content="{{Request::url()}}" />
+    <meta property="og:title"  content="Knocks" />
+    <meta property="og:image"  content="https://knocksapp.com/snaps/knocks_logo.png" />
+    <meta property="og:description" content="Knocks, Reimagining the social media.">
+    <meta property="og:site_name" content="Knocks, Inc.">
+    <!--Google-->
+    <meta name="google-site-verification" content="tsUIFUH4rGfxzV0M-SOdbUhgUTrwisWXmhNZpda_pVk" />
+    <meta name="robots" content="follow"/>
     <meta charset="UTF-8" />
+    <meta name="title" content="Knocks">
+    <meta name="author" content="Knocks Development Team">
+    <meta name="publisher" content="Knocks, INC">
+    <meta name="copyright" content="Knocks 2017">
+    <meta name="description" content="Knocks, Reimagining The Social Media">
+    <meta name="abstract" content="Knocks, Reimagining The Social Media">
+    <meta http-equiv="content-language" content="en">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="robots" content="index,follow">
+    <meta name="DC.Language" content="en">
+    <meta name="keywords" lang="en" content="knocks">
+    <meta name="keywords" lang="en" content="knocksapp">
+    <meta name="keywords" lang="en" content="register">
+    <meta name="keywords" lang="en" content="signin">
+    <meta name="keywords" lang="en" content="login">
+    <meta name="keywords" lang="en" content="survey">
+    <meta name="keywords" lang="en" content="social media">
+    <meta name="keywords" lang="en" content="social">
+    <meta name="keywords" lang="en" content="media">
+    <meta name="keywords" lang="en" content="analysis">
+    <meta name="keywords" lang="en" content="statistics">
     <div id = "knocks_nav_vue"></div>
     <!--Internet Explorer Comp -->
     <link rel="stylesheet" type="text/css" href="{{asset('css/animate.css')}}">
@@ -36,80 +58,68 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="user" content="-1">
     <meta name="session-type" content="guest">
-    <meta name="description" content="Knocks, Reimagining the social media.">
     <meta name="keywords" content="Knocks,KnocksApp,Who's There,Social Media,social media,social,knocksapp,knocks,survey,Survey,survey,Survey,answers,knocksapp,KnocksApp,knocksapp.com">
     <meta name="author" content="KnocksApp">
-
-    <meta property="og:description" content="Knocks, Reimagining the social media.">
-
-
-    <meta property="og:site_name" content="Knocks, Inc.">
     <link rel = "stylesheet" href = {{asset('css/app.css')}}  />
     @yield('externals')
   </head>
   <body class = "knocks_color_kit">
-
     <main style="padding-left:0px !important; padding-right: 0px !important; ">
       <div id = "app">
-     <div>
-       @yield('content')
-
-
-
-
-       </div>
+        <div>
+          @yield('content')
+        </div>
       </div>
     </main>
   </body>
   @include('layouts.footer_dark')
-   <script>
+  <script>
   window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1796023703741381',
-      xfbml      : true,
-      version    : 'v2.12'
-    });
+  FB.init({
+  appId      : '1796023703741381',
+  xfbml      : true,
+  version    : 'v2.12'
+  });
   
-    FB.AppEvents.logPageView();
+  FB.AppEvents.logPageView();
   
   };
-
   (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  </script>
   <script type="application/ld+json">
-{
+  {
   "@context": "http://schema.org",
   "@type": "Organization",
   "url": "http://www.knocksapp.com",
   "name": "Knocks.",
   "logo": "https://knocksapp.com/ssnaps/knocks_logo.png" ,
   "legalName" : "Knocks, INC",
-   "foundingDate": "2017",
-    "founders": [
- {
- "@type": "Person",
- "name": "M.Amr Moussa"
- },
- {
- "@type": "Person",
- "name": "Hesham Ahmed"
- } ,
- {
- "@type": "Person",
- "name": "Salma Roshdy"
- } ,
- {
- "@type": "Person",
- "name": "Khaled Ashraf"
- } 
- ]
-}
-</script>
+  "foundingDate": "2017",
+  "founders": [
+  {
+  "@type": "Person",
+  "name": "M.Amr Moussa"
+  },
+  {
+  "@type": "Person",
+  "name": "Hesham Ahmed"
+  } ,
+  {
+  "@type": "Person",
+  "name": "Salma Roshdy"
+  } ,
+  {
+  "@type": "Person",
+  "name": "Khaled Ashraf"
+  }
+  ]
+  }
+  </script>
   <script src = "{{asset('js/app.js')}}"></script>
 </html>
